f0f1311e56b04237f9c3006fd699c131858ed059
Looper <aayush.bhattacharjee2002@gmail.com>
2021-07-09 17:55:38 +0530

    added bases of buttons

--- todogui/controller.java
+++ todogui/controller.java
@@ -8,7 +8,12 @@ import javafx.fxml.FXML;
 public class controller {
     @FXML
     public void add(ActionEvent e) throws IOException{
+
+    }
+    public void showTasks(ActionEvent e) throws Exception{
+
+    }
+    public void removeTask(ActionEvent e) throws Exception{
         
     }
-    
 }
