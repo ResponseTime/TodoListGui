54b17d9938018f9abacf5815c0731beee64d9c0f
Looper <aayush.bhattacharjee2002@gmail.com>
2021-07-09 21:52:40 +0530

    Connected listview to database

--- todogui/controller.java
+++ todogui/controller.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TextField;
 
 public class controller {
     private static Statement stmt;
+    private static ResultSet r;
     public static void con() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3308/todo", "root","root");
@@ -24,6 +25,12 @@ public class controller {
     @FXML
     ListView<String> shown;
     @FXML
+    public void showTasks(ActionEvent e) throws Exception{
+        r = stmt.executeQuery("SELECT TASK FROM TODO");
+        while(r.next()){
+            shown.getItems().add(r.getString(1));
+        }
+    }
     public void add(ActionEvent e) throws Exception{
         tk = task.getText();
         con();
@@ -33,14 +40,7 @@ public class controller {
         shown.getItems().clear();
         return;
     }
-    public void showTasks(ActionEvent e) throws Exception{
-            for(int i =0;i<to.size();i++){
-                shown.getItems().add(to.get(i));
-            }
-            
-     
-        
-    }
+    
     public void removeTask(ActionEvent e) throws Exception{
 
     }
