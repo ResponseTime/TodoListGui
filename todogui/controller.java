f2fc35b7a0757fb00c799940322d109dc3d0eb89
Looper <aayush.bhattacharjee2002@gmail.com>
2021-07-09 08:33:35 -0700

    added alot of features

--- todogui/controller.java
+++ todogui/controller.java
@@ -1,17 +1,44 @@
 package todogui;
 
-import java.io.IOException;
 
+import java.util.ArrayList;
+import java.sql.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-public class controller {
-    @FXML
-    public void add(ActionEvent e) throws IOException{
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
+public class controller {
+    private static Statement stmt;
+    public static void con() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3308/todo", "root","root");
+        stmt = con.createStatement();
+    }
+    private String tk;
+    private static ArrayList<String> to = new ArrayList<String>();
+    private static int count=1;
+    @FXML
+    TextField task;
+    @FXML
+    ListView<String> shown;
+    @FXML
+    public void add(ActionEvent e) throws Exception{
+        tk = task.getText();
+        con();
+        to.ensureCapacity(500);
+        to.add(tk);
+        stmt.executeUpdate("INSERT INTO TODO VALUES ('"+count++ +"','"+tk+"')");
+        return;
     }
     public void showTasks(ActionEvent e) throws Exception{
-
+            for(int i =0;i<to.size()+1;i++){
+                shown.getItems().add(to.get(i));
+            }
+            
+     
+        
     }
     public void removeTask(ActionEvent e) throws Exception{
         
